83a49f6680fa6e35b2c14d0b7825168c80ac6f13
Shiran Eyal <shiran.eyal@gmail.com>
2024-08-16 04:37:29 +0300

    added a bunch of constants

--- GithubConfigAutomation.py
+++ GithubConfigAutomation.py
@@ -19,6 +19,27 @@ ForcePushDisabledSuccess = "Successfully disabled force pushing for branch '{}'.
 ForcePushDisabledFailure = "Failed to update protection settings: {}"
 ForcePushDisabledFinished = "Finished removing force pushing from all protected branches."
 
+# Protection field strings
+REQUIRED_STATUS_CHECKS = "required_status_checks"
+ENFORCE_ADMINS = "enforce_admins"
+REQUIRED_PULL_REQUEST_REVIEWS = "required_pull_request_reviews"
+RESTRICTIONS = "restrictions"
+ALLOW_FORCE_PUSHES = "allow_force_pushes"
+REQUIRED_LINEAR_HISTORY = "required_linear_history"
+ALLOW_DELETIONS = "allow_deletions"
+BLOCK_CREATIONS = "block_creations"
+REQUIRED_CONVERSATION_RESOLUTION = "required_conversation_resolution"
+LOCK_BRANCH = "lock_branch"
+ALLOW_FORK_SYNCING = "allow_fork_syncing"
+STRICT = "strict"
+CONTEXTS = "contexts"
+ENABLED = "enabled"
+
+URL_P1 = "https://api.github.com/repos/"
+URL_P2 = "/"
+URL_P3 = "/branches/"
+URL_P4 = "/protection"
+
 # This function verifies that the repository`s default branch is configured properly for pull request reviewers.
 # If it is not we create a new protection ruleset for it.
 def ConfigurePullReqReviewers(repo, curr_user):
@@ -32,17 +53,12 @@ def ConfigurePullReqReviewers(repo, curr_user):
     if not protection or not protection.required_pull_request_reviews or \
             protection.required_pull_request_reviews.required_approving_review_count == 0:
         print(PullReqNeedConfig.format(default_branch.name))
-
-        url = f"https://api.github.com/repos/{curr_user.login}/{repo.name}/branches/{default_branch.name}/protection"
-        headers = {
-            "Authorization": f"token {ACCESS_TOKEN}",
-            "Accept": "application/vnd.github.v3+json",
-            "Content-Type": "application/json"
-        }
+        url = URL_P1 + curr_user.login + URL_P2 + repo.name + URL_P3 + default_branch.name + URL_P4
+        headers = GetReqHeaders()
         # Get current protection settings
         updated_settings = GetBranchProtectionSettings(url, headers)
-        updated_settings["required_pull_request_reviews"] = {
-            "required_approving_review_count": REQ_REVIEWS
+        updated_settings[REQUIRED_PULL_REQUEST_REVIEWS] = {
+            REQUIRED_PULL_REQUEST_REVIEWS: REQ_REVIEWS
         }
 
         # Make put request to update protection settings
@@ -57,20 +73,15 @@ def ConfigurePullReqReviewers(repo, curr_user):
 # This function verifies that all branches in the repository that have some sort of protection rule have the option
 # to force push turned off.
 def DisableForcePushing(repo, curr_user):
-    allow_force_pushes = "allow_force_pushes"
     for branch in repo.get_branches():
         try:
             # Try to get protection. if failed then this is an unprotected branch and we can continue
             protection = branch.get_protection()
-            url = f"https://api.github.com/repos/{curr_user.login}/{repo.name}/branches/{branch.name}/protection"
-            headers = {
-                "Authorization": f"token {ACCESS_TOKEN}",
-                "Accept": "application/vnd.github.v3+json",
-                "Content-Type": "application/json"
-            }
+            url = URL_P1 + curr_user.login + URL_P2 + repo.name + URL_P3 + branch.name + URL_P4
+            headers = GetReqHeaders()
             # Get current protection settings
             updated_settings = GetBranchProtectionSettings(url, headers)
-            updated_settings[allow_force_pushes] = False
+            updated_settings[ALLOW_FORCE_PUSHES] = False
 
             # Make put request to update protection settings
             response = requests.put(url, headers=headers, data=json.dumps(updated_settings))
@@ -81,6 +92,14 @@ def DisableForcePushing(repo, curr_user):
         except:
             continue
     print(ForcePushDisabledFinished)
+
+# Helper function to get header for requests
+def GetReqHeaders():
+    return {
+                "Authorization": f"token {ACCESS_TOKEN}",
+                "Accept": "application/vnd.github.v3+json",
+                "Content-Type": "application/json"
+            }
 
 # Helper function to retrieve the protection settings of a branch using specified url and headers
 def GetBranchProtectionSettings(url, headers):
@@ -93,27 +112,27 @@ def GetBranchProtectionSettings(url, headers):
     protection_settings = response.json()
 
     # Reconfigure required_status_checks field
-    required_status_checks = protection_settings.get('required_status_checks', None)
+    required_status_checks = protection_settings.get(REQUIRED_STATUS_CHECKS, None)
     if required_status_checks is not None:
         required_status_checks = {
-            "strict": required_status_checks.get("strict", True),
-            "contexts": required_status_checks.get("contexts", [])
+            STRICT: required_status_checks.get(STRICT, True),
+            CONTEXTS: required_status_checks.get(CONTEXTS, [])
         }
 
     # Prepare updated settings with default values if not present
     updated_settings = {
-        "required_status_checks": required_status_checks,
-        "enforce_admins": protection_settings.get('enforce_admins', {}).get('enabled', True),
-        "required_pull_request_reviews": protection_settings.get('required_pull_request_reviews', None),
-        "restrictions": protection_settings.get('restrictions', None),
-        "allow_force_pushes": protection_settings.get('allow_force_pushes', {}).get('enabled', False),
-        "required_linear_history": protection_settings.get('required_linear_history', {}).get('enabled', False),
-        "allow_deletions": protection_settings.get('allow_deletions', {}).get('enabled', False),
-        "block_creations": protection_settings.get('block_creations', {}).get('enabled', False),
-        "required_conversation_resolution": protection_settings.get('required_conversation_resolution', {}).get(
-            'enabled', False),
-        "lock_branch": protection_settings.get('lock_branch', {}).get('enabled', False),
-        "allow_fork_syncing": protection_settings.get('allow_fork_syncing', {}).get('enabled', False)
+        REQUIRED_STATUS_CHECKS: required_status_checks,
+        ENFORCE_ADMINS: protection_settings.get(ENFORCE_ADMINS, {}).get(ENABLED, True),
+        REQUIRED_PULL_REQUEST_REVIEWS: protection_settings.get(REQUIRED_PULL_REQUEST_REVIEWS, None),
+        RESTRICTIONS: protection_settings.get(RESTRICTIONS, None),
+        ALLOW_FORCE_PUSHES: protection_settings.get(ALLOW_FORCE_PUSHES, {}).get(ENABLED, False),
+        REQUIRED_LINEAR_HISTORY: protection_settings.get(REQUIRED_LINEAR_HISTORY, {}).get(ENABLED, False),
+        ALLOW_DELETIONS: protection_settings.get(ALLOW_DELETIONS, {}).get(ENABLED, False),
+        BLOCK_CREATIONS: protection_settings.get(BLOCK_CREATIONS, {}).get(ENABLED, False),
+        REQUIRED_CONVERSATION_RESOLUTION: protection_settings.get(REQUIRED_CONVERSATION_RESOLUTION, {}).get(
+            ENABLED, False),
+        LOCK_BRANCH: protection_settings.get(LOCK_BRANCH, {}).get(ENABLED, False),
+        ALLOW_FORK_SYNCING: protection_settings.get(ALLOW_FORK_SYNCING, {}).get(ENABLED, False)
     }
     return updated_settings
 
@@ -123,4 +142,4 @@ if __name__ == "__main__":
     curr_user = g.get_user()
     repo = curr_user.get_repo(REPO_NAME)
     ConfigurePullReqReviewers(repo, curr_user)
-    # DisableForcePushing(repo, curr_user)
+    DisableForcePushing(repo, curr_user)
